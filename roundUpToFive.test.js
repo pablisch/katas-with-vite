6d4a0fd17dd4fa183cdcfcc86cafe194aa735fd0
Pablo <pablito@me.com>
2024-02-19 20:33:22 +0000

    No changes

--- roundUpToFive.test.js
+++ roundUpToFive.test.js
@@ -1,5 +1,5 @@
-ççimport { describe } from 'vitest';
-import {roundUpToFive} from './roundUpToFive';
+import { describe } from 'vitest';
+import { roundUpToFive } from './roundUpToFive';
 
 describe('roundUpToFive()', () => {
   test.each([
@@ -10,7 +10,10 @@ describe('roundUpToFive()', () => {
     [30, 30],
     [-2, 0],
     [-5, -5],
-  ])('should return the the input rounded up to the nearest mulitple of 5', (num, expectedNum) => {
-    expect(roundUpToFive(num)).toBe(expectedNum)
-  })
-})
+  ])(
+    'should return the the input rounded up to the nearest mulitple of 5',
+    (num, expectedNum) => {
+      expect(roundUpToFive(num)).toBe(expectedNum);
+    }
+  );
+});
