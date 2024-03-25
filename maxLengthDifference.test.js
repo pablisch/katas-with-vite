757728d40aeb557bd93cb4f50d0b80f10997bd2e
Pablo <pablito@me.com>
2024-03-25 13:21:43 +0000

    Added a test file to see if I could create a func and test it in the same file.

--- maxLengthDifference.test.js
+++ maxLengthDifference.test.js
@@ -1,13 +1,13 @@
-import { getMaxDiff } from "./maxLengthDifference";
+// import { getMaxDiff } from "./maxLengthDifference";
 
-describe('getMaxDiff()', () => {
-  test.each([
-    [["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"], '13']
-  ])('should return a string of the maximum difference in length between the strings in the two input strings', (arr1, arr2, expectedResult) => {
-    // Act
-    const result = getMaxDiff(arr1, arr2);
+// describe('getMaxDiff()', () => {
+//   test.each([
+//     [["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"], '13']
+//   ])('should return a string of the maximum difference in length between the strings in the two input strings', (arr1, arr2, expectedResult) => {
+//     // Act
+//     const result = getMaxDiff(arr1, arr2);
 
-    // Assert
-    expect(result).toBe(expectedResult);
-  })
-})
+//     // Assert
+//     expect(result).toBe(expectedResult);
+//   })
+// })
